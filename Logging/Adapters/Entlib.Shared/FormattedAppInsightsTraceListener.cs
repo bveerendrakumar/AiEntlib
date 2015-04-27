d5900d1e1122c03a25e04176574baad3217e3493
Veerendra Kumar Balla <vballa@microsoft.com>
2015-04-27 11:29:38 +0530

    test switch

--- Logging/Adapters/Entlib.Shared/FormattedAppInsightsTraceListener.cs
+++ Logging/Adapters/Entlib.Shared/FormattedAppInsightsTraceListener.cs
@@ -5,7 +5,7 @@
 // THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY
 // OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT
 // LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
-// FITNESS FOR A PARTICULAR PURPOSE.
+// FITNESS FOR A PARTICULAR PURPOSE.  Test
 // ==============================================================================
 // </copyright>
 // -----------------------------------------------------------------------
